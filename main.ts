041ba76c51fd24c93b97a203718acf283d13b249
Miki-L21 <122608792+Miki-L21@users.noreply.github.com>
2023-01-14 10:36:43 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+let mão = 0
+input.onGesture(Gesture.Shake, function () {
+    mão = randint(1, 3)
+    if (mão == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (mão == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            # # . . .
+            # # # . #
+            . . . # .
+            # # # . #
+            # # . . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
